65bc79321cbb0bab03ee31a11c304fe228a9af09
Ntombizanele <zanelemoni4@gmail.com>
2022-04-14 00:01:45 +0200

    Add files via upload

---
+++ src/components/About.vue
@@ -0,0 +1,144 @@
+<template>
+  <section class="about py-5" id="about">
+    <div class="container-lg py-4">
+      <div class="row justify-content-center">
+        <div class="col-lg-8">
+          <div class="section-title text-center">
+            <h2 class="fw-bold mb-5">About me</h2>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6 p-5">
+          <div class="about-text">
+            <h3 class="fs-4 mb-3">Introduction</h3>
+            <p class="text-black">
+              I am a hard working individual, I am always willing to learn and
+              to help others,My communication and organizational skills are
+              exceptional, as well as my ability to work under pressure even in
+              times of change,I'm keen to develop a new skill and use existing
+              skills to perform a variety of tasks successfully,If given an
+              opportunity I believe that I can be a great asset to any team.
+              <br /><br />
+
+              Full name: Ntombizanele Moni <br />
+              Date of birth: 1997/12/25 <br />
+              Location: South Africa,Cape Town <br />
+              Cell Phone number: 0624334358
+            </p>
+          </div>
+          <div class="row">
+            <div class="col-lg-12 d-flex align-items-center p-3">
+              <div class="social-links">
+                <a href="https://github.com/ZaneleMoni" class="text-dark me-2"
+                  target="_blank"><i class="fab fa-github"></i
+                ></a>
+                <a href="https://codepen.io/ZaneleMoni" class="text-dark me-2"
+                 target="_blank" ><i class="fab fa-codepen"></i
+                ></a>
+                <a
+                  href="https://www.linkedin.com/in/zanele-moni-b88559184/"
+                  class="text-dark me-2"
+                 target="_blank" ><i class="fab fa-linkedin-in"></i
+                ></a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 mt-5 mt-md-0 p-5">
+          <div class="skill-item mb-4">
+            <h3 class="fs-6">HTML/CSS</h3>
+            <div class="progress" style="height: 5px">
+              <div
+                class="progress-bar bg-danger"
+                role="progressbar"
+                style="width: 75%"
+                aria-valuenow="75"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+          <div class="skill-item mb-4">
+            <h3 class="fs-6">JavaScript</h3>
+            <div class="progress" style="height: 5px">
+              <div
+                class="progress-bar bg-danger"
+                role="progressbar"
+                style="width: 65%"
+                aria-valuenow="65"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+          <div class="skill-item mb-4">
+            <h3 class="fs-6">Vue.js</h3>
+            <div class="progress" style="height: 5px">
+              <div
+                class="progress-bar bg-danger"
+                role="progressbar"
+                style="width: 60%"
+                aria-valuenow="60"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+          <div class="skill-item mb-4">
+            <h3 class="fs-6">Bootstrap</h3>
+            <div class="progress" style="height: 5px">
+              <div
+                class="progress-bar bg-danger"
+                role="progressbar"
+                style="width: 80%"
+                aria-valuenow="80"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+          <div class="skill-item mb-4">
+            <h3 class="fs-6">Node.js</h3>
+            <div class="progress" style="height: 5px">
+              <div
+                class="progress-bar bg-danger"
+                role="progressbar"
+                style="width: 75%"
+                aria-valuenow="80"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+          <div class="skill-item mb-4">
+            <h3 class="fs-6">Mongo.DB</h3>
+            <div class="progress" style="height: 5px">
+              <div
+                class="progress-bar bg-danger"
+                role="progressbar"
+                style="width: 90%"
+                aria-valuenow="80"
+                aria-valuemin="0"
+                aria-valuemax="100"
+              ></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <div class="container">
+    <hr />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+section {
+  height: 100vh;
+}
+</style>
